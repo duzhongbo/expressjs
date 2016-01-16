29b81b740181428511fade181f7c8d900493bf7a
duzhongbo <710409599@qq.com>
2016-01-16 23:41:38 +0800

    混合使用函数和函数数组处理路由：

--- myapp/app.js
+++ myapp/app.js
@@ -50,6 +50,25 @@ var cb2 = function (req, res) {
 
 app.get('/example/c', [cb0, cb1, cb2]);
 
+// 混合使用函数和函数数组处理路由：
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+}
+
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+}
+
+app.get('/example/d', [cb0, cb1], function (req, res, next) {
+  console.log('response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from D!');
+});
+
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
